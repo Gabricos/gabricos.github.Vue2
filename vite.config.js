dfad8ce12ca9fb59c1ea86c8cd6c48ff5e7e73cd
GabrieleCostanza <gabrielecostanzawork@gmail.com>
2025-09-18 16:28:03 +0200

    Altro commit per fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/gabricos.github.Vue2/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  }
+  base: '/gabricos.github.Vue2/'
 })
